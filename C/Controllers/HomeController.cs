8ac34c4d10f41f5174d8cff19f50245e4aaa3311
1101137223 <davidnini10@gmail.com>
2015-06-27 00:15:46 +0800

    Final

---
+++ C/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using C.Models;
+
+namespace C.Controllers
+{
+    public class HomeController : Controller
+    {
+        private STUEntities db = new STUEntities();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index(int? id)
+        {
+            return View(db.student.ToList());
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            student student = db.student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.student.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            student student = db.student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            student student = db.student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            student student = db.student.Find(id);
+            db.student.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ C/Controllers/addController.cs
@@ -0,0 +1,38 @@
+﻿using C.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace C.Controllers
+{
+    public class addController : Controller
+    {
+        //
+        // GET: /add/
+        private STUEntities db = new STUEntities();
+        public ActionResult Index()
+        {
+                        return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        public ActionResult Create(FormCollection form, int stuID, int subID, string arr)
+        {
+            
+            var record = new record
+            {
+                record_ID = 2,
+                class_ID = subID,
+                stu_ID = stuID,
+                arrive = arr
+            };
+            db.record.Add(record);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
